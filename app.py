d0e0ec4b08bb5cdc17e7d9164a7884b36dd86c2b
Rafael Almeida <rafael.rodrigo.p.a@gmail.com>
2026-02-18 16:46:15 -0300

    Teste comparativo com funcao passando o valor da coluna que deseja comparar

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from comparativos import comparativo_captacao
+import numpy as np
 
-print(comparativo_captacao())
+teste = comparativo_captacao("data_doc")['diferenca']
+teste_2 = np.where(teste > 0, 'Positivo', 'Negativo')
+
+print(teste_2)
--- comparativos.py
+++ comparativos.py
@@ -2,7 +2,7 @@ import pandas as pd
 from db import engine
 from conn_ftp import conect_ftp_e_leitura
 
-def comparativo_captacao():
+def comparativo_captacao(coluna_comparativa="doc_vendas"):
     query_captacao = """ 
         SELECT 
             *
@@ -14,7 +14,7 @@ def comparativo_captacao():
     df_captacao = pd.read_sql(query_captacao,engine)
 
     # Agrupado por doc_vendas -> Valor liquido
-    df_captacao_agrupado = df_captacao.groupby('doc_vendas')['valor_liquido'].sum()
+    df_captacao_agrupado = df_captacao.groupby(coluna_comparativa)['valor_liquido'].sum()
     df_captacao_agrupado = df_captacao_agrupado.to_frame()
     df_captacao_agrupado = df_captacao_agrupado.reset_index()
 
@@ -24,7 +24,7 @@ def comparativo_captacao():
     df = bases['captacao_pedidos_202602.xlsx']
 
     # Agrupado por doc_venda -> Valor liquido
-    df_agrupado = df.groupby('doc_vendas')['valor_liquido'].sum()
+    df_agrupado = df.groupby(coluna_comparativa)['valor_liquido'].sum()
     df_agrupado = df_agrupado.to_frame()
     df_agrupado = df_agrupado.reset_index()
 
@@ -33,8 +33,8 @@ def comparativo_captacao():
     docs_unicos = (
         pd.concat([
             # transformando no tipo string
-            df_agrupado["doc_vendas"].astype(str),
-            df_captacao_agrupado["doc_vendas"].astype(str)
+            df_agrupado[coluna_comparativa].astype(str),
+            df_captacao_agrupado[coluna_comparativa].astype(str)
         ])
         # Remove duplicatas
         .drop_duplicates()
@@ -43,7 +43,7 @@ def comparativo_captacao():
         .reset_index(drop=True)
     )
     # Ao juntar e remover duplicatas transformou e Series, por isso é preciso converter para dataframe
-    docs_unicos = docs_unicos.to_frame(name="doc_vendas")
+    docs_unicos = docs_unicos.to_frame(name=coluna_comparativa)
 
     #print(docs_unicos)
 
@@ -51,19 +51,19 @@ def comparativo_captacao():
     #LEFT JOIN: base -> SQL -> FTP
 
     # padroniza tipo
-    docs_unicos["doc_vendas"] = docs_unicos["doc_vendas"].astype(str).str.strip()
-    df_agrupado["doc_vendas"] = df_agrupado["doc_vendas"].astype(str).str.strip()
-    df_captacao_agrupado["doc_vendas"] = df_captacao_agrupado["doc_vendas"].astype(str).str.strip()
+    docs_unicos[coluna_comparativa] = docs_unicos[coluna_comparativa].astype(str).str.strip()
+    df_agrupado[coluna_comparativa] = df_agrupado[coluna_comparativa].astype(str).str.strip()
+    df_captacao_agrupado[coluna_comparativa] = df_captacao_agrupado[coluna_comparativa].astype(str).str.strip()
 
     # Realiza o merge do arquivo FTP com os dados da Tabela do SQL
     df_merge = docs_unicos.merge(
         df_agrupado,
-        on="doc_vendas",
+        on=coluna_comparativa,
         how="left",
         suffixes=("", "_ftp")
     ).merge(
         df_captacao_agrupado,
-        on="doc_vendas",
+        on=coluna_comparativa,
         how="left",
         suffixes=("", "_sql")
     )
@@ -80,7 +80,7 @@ def comparativo_captacao():
 
     filtro_docs_divergentes = df_merge[df_merge['status']=='DIVERGENTE']
 
-    return filtro_docs_divergentes
+    return filtro_docs_divergentes[filtro_docs_divergentes['data_doc']=='2026-02-01']
     #print(filtro_docs_divergentes)
 
 
